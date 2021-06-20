f33fd4b63f168d807dce618680b32aa4093789b1
Miglena Dimitrova <mimas4emn@gmail.com>
2021-06-20 09:19:03 +0300

    Implement UsersController and UsersService

--- Apps/Git/Controllers/UsersController.cs
+++ Apps/Git/Controllers/UsersController.cs
@@ -1,10 +1,107 @@
-﻿using System;
+﻿using Git.Services;
+using Git.ViewModels;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Git.Controllers
 {
-    public class UsersController
+    public class UsersController: Controller
     {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+        public HttpResponse Login()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(LoginInputModel input)
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+            var userId = this.usersService.GetUserId(input.Username, input.Password);
+
+            if (userId==null)
+            {
+                return this.Error("Invalid username or password.");
+            }
+            this.SignIn(userId);
+            return this.Redirect("/Repositories/All");
+        }
+
+        public HttpResponse Register()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(RegisterInputModel input)
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+            if (String.IsNullOrWhiteSpace( input.Username)||input.Username.Length<5|| input.Username.Length > 20)
+            {
+                return this.Error("Username must be between 5 and 20 characters long!");
+            }
+
+            if (string.IsNullOrWhiteSpace(input.Email) || !new EmailAddressAttribute().IsValid(input.Email))
+            {
+                return this.Error("Invalid email!");
+            }
+
+            if (String.IsNullOrWhiteSpace(input.Password) || input.Password.Length < 6 || input.Password.Length > 20)
+            {
+                return this.Error("Password must be between 6 and 20 characters long!");
+            }
+
+            if (input.Password != input.ConfirmPassword)
+            {
+                return this.Error("Confirmed password should be the same!");
+            }
+
+            if (!this.usersService.IsEmailAvailable(input.Email))
+            {
+                return this.Error("Email already taken!");
+            }
+
+            if (!this.usersService.IsUsernameAvailable(input.Username))
+            {
+                return this.Error("Username is not available!");
+            }
+
+            this.usersService.CreateUser(input.Username,input.Email, input.Password);
+            return this.Redirect("/Users/Login");
+        }
+
+        public HttpResponse Logout()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Error("Only logged-in users can logout");
+            }
+            this.SignOut();
+            return this.Redirect("/");
+        }
+
     }
 }
