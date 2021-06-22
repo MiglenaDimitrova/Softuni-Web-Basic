8297f2ff1bccf8311491b3f60a361ccfced1ab01
Miglena Dimitrova <mimas4emn@gmail.com>
2021-06-22 08:04:32 +0300

    Implement Users Controller and Service

--- Apps/CarShop/Services/UsersService.cs
+++ Apps/CarShop/Services/UsersService.cs
@@ -1,5 +1,5 @@
 ﻿using CarShop.Data;
-
+using CarShop.Data.Models;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -8,25 +8,56 @@ namespace CarShop.Services
 {
     public class UsersService: IUsersService
     {
+        private readonly ApplicationDbContext db;
+
+        public UsersService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
 
         public void Create(string username, string email, string password, string userType)
         {
-            throw new System.NotImplementedException();
+            var user = new User
+            {
+                Username = username,
+                Email = email,
+                Password = ComputeHash(password),
+                IsMechanic = userType == "Mechanic"
+            };
+            this.db.Users.Add(user);
+            this.db.SaveChanges();
         }
 
         public string GetUserId(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var computePassword = ComputeHash(password);
+            var user =  this.db.Users.FirstOrDefault(x => x.Username == username && x.Password == computePassword);
+            return user?.Id;
         }
-
         public bool IsUserMechanic(string Userid)
         {
-            throw new System.NotImplementedException();
+            return this.db.Users.FirstOrDefault(x => x.Id == Userid).IsMechanic;
         }
 
         public bool IsUsernameAvailable(string username)
         {
-            throw new System.NotImplementedException();
+            return !this.db.Users.Any(x => x.Username == username);
+        }
+        
+        private static string ComputeHash(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                // Convert to text
+                // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+                var hashedInputStringBuilder = new StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
         }
     }
 }
