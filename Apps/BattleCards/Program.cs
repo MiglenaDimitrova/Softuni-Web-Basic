b81fec854a1df81e80dc9366b8e63ebb4f8af4a5
Miglena Dimitrova <mimas4emn@gmail.com>
2021-06-25 07:55:21 +0300

    Add Skeleton and implement DB

---
+++ Apps/BattleCards/Program.cs
@@ -0,0 +1,15 @@
+﻿using BattleCards;
+using SUS.MvcFramework;
+using System;
+using System.Threading.Tasks;
+
+namespace BattleCard
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            await Host.CreateHostAsync(new Startup());
+        }
+    }
+}
---
+++ Apps/BattleCards/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace BattleCards
+{
+    using System.Collections.Generic;
+    using BattleCards.Data;
+    using Microsoft.EntityFrameworkCore;
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+
+    public class Startup : IMvcApplication
+    {
+        public void Configure(List<Route> routeTable)
+        {
+            new ApplicationDbContext().Database.Migrate();
+        }
+
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            
+        }
+    }
+}
